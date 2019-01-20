798e55c6b1fa8cf70a7ab0857217f6f5979638dd
Lin Weizhe Dragon <dvmsc110@gmail.com>
2019-01-20 02:32:22 +0000

    update

--- src/website/EasyEye/api/views.py
+++ src/website/EasyEye/api/views.py
@@ -1,23 +1,71 @@
 import json
+import pytz
+from datetime import datetime, timedelta
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import JsonResponse
+from api.models import SecondData, Alert
 
 # Create your views here.
 
 
 class GetAlertView(TemplateView):
     # API call processing
-    template_name = 'live_search/index.html'
 
     def get(self, request, *args, **kwargs):
         #received_json_data = json.loads(request.body)
+        try:
+            alerts = Alert.objects.all().order_by('-start_time')
+            if alerts.count() == 0:
+                return JsonResponse({
+                    'msg': 'Return Successfully',
+                    'alert': [],
+                })
+            else:
+                alert = alerts[0]
+            return JsonResponse({
+                'msg': 'Return Successfully',
+                'alert': [
+                    alert.alert_wrong_direction,
+                    alert.alert_blink_slow,
+                    alert.alert_blur_sight,
+                    alert.alert_usage_overtime,
+                          ],
+                'timestamp': alert.start_time.strftime('%Y-%m-%d %H:%M:%S.%f'),
+            })
+        except:
+            return JsonResponse({
+                'msg': 'Fail',
+            })
 
-        data = {
-            'msg':'Success',
-            #'data':received_json_data,
-        }
+class DataUploadView(TemplateView):
+    # API call processing
 
-        return JsonResponse({
-            'data': data,
-        })
+    def get(self, request, *args, **kwargs):
+
+        try:
+            timestamp = request.GET['timestamp']
+            data = request.GET.getlist('data[]')
+            new_record = SecondData()
+            start_time = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
+            start_time = pytz.timezone('UTC').localize(start_time)
+            #print(start_time)
+            new_record.start_time = start_time
+            new_record.blink = data[0]
+            new_record.direction = data[1]
+            new_record.squint = data[2]
+            if SecondData.objects.filter(start_time__gt=datetime.utcnow() - timedelta(minutes=1)).count() == 0:
+                new_record.start_point = True
+            print(new_record)
+            new_record.save()
+
+            return JsonResponse({
+                'msg': 'Success',
+            })
+        except:
+            return JsonResponse({
+                'msg': 'Fail',
+            })
+
+
+
